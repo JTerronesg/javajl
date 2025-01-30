a421a014061d147467e63693c57314d42bce004b
sl75599888@idat.pe <sl75599888@idat.pe>
2025-01-30 09:14:20 -0500

    Cambios en el campo salida por Emerson

--- src/semana3/frmUniversidad.java
+++ src/semana3/frmUniversidad.java
@@ -51,6 +51,7 @@ public class frmUniversidad extends javax.swing.JFrame {
         txtSalida.append("Pensión Actual: " + actPen);
         txtSalida.append("Descuento: " + desc);
         txtSalida.append("Nueva Pensión: " + nuevaPen);
+        txtSalida.append(" cici 30 so");
         
     }
     
